f905e52e56f85314809cc278f58f830c1fa7532e
Nicholas Ilacqua <Niservices88@gmail.com>
2021-10-21 15:17:10 -0500

    Worked out issues w/Matt, Thanks Mattgit status

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,11 +14,11 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName( ) {
-  return 'Jeff';
+function helloName(name) {
+  return ` Hello, ${name}`
 }
 // Remember to call the function to test
-console.log('Hello', helloName());
+console.log( helloName('nick'));
 
 // 3. Function to add two numbers together & return the result
 function addNumbers( firstNumber, secondNumber ) {
@@ -30,11 +30,12 @@ function addNumbers( firstNumber, secondNumber ) {
 console.log('numbers 1 + 2 =', addNumbers(1, 2));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-let answer = 5 * 5 * 5;
+function multiplyThree(firstNumber, secondNumber, thirdNumber){
+let answer = firstNumber * secondNumber * thirdNumber;
 return answer;
 }
-console.log('multipling 5 three times =', multiplyThree());
+console.log('multipling numbers =', multiplyThree(2, 2, 2));
+console.log('multipling numbers =', multiplyThree(50, 100, 100));
 
 // 5. Function that will return true if a number is positive,
 //    or greater than zero, and false otherwise
@@ -53,28 +54,28 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 
 // 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
-let item =[];
-function getLast( array ) {
-  return item.at(-1);
+let emptyarray =[];
+let testarray =  ['ball', 'frog', 'bird']
+function getLast(array) {
+  return array.at(-1);
 }
-item.push('pizza');
-item.push('carrots');
-console.log('The last item in the cart is', getLast(item));
-
+console.log('The last item in the cart is', getLast(emptyarray));
+console.log('The last item in the cart is', getLast(testarray));
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find
 function find( value, array ){
-for (let i = 0; i < item.length; i++) {
-  if (item [i] === value) {
-    return true;
-  } else {
-    return false;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return true;
+    }
   }
-
+  return false;
 }
-}
-console.log('find the value', find(item));
+console.log('find the value', find('ball', testarray));
+console.log('find the value', find('fox', testarray));
+console.log('find the value', find('frog', testarray));
+console.log('find the value', find('bird', testarray));
 // ----------------------
 // Stretch Goals
 // ----------------------
